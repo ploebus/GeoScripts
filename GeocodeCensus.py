3990e58d2931bfd3bc431fc0f887196d96817fef
ploebus <johng@urbanstrategies.org>
2020-02-03 09:52:42 -0800

    Update GeocodeCensus.py

--- GeocodeCensus.py
+++ GeocodeCensus.py
@@ -9,23 +9,6 @@ BASE_URL = 'https://geocoding.geo.census.gov/geocoder/geographies/onelineaddress
 
 result_list = []
 
-
-def getTRACT(lat, lng):
-    try:
-        resp = requests.get('http://data.fcc.gov/api/block/find?', params={'latitude': lat, 'longitude': lng, 'format': 'json'})
-        data = resp.json()
-        print(data)
-        if data['Block']['FIPS'][5:11]:
-            return data['Block']['FIPS'][5:11]
-        else: 
-            return '99'
-    except:
-            return '99'
-
-
-
-
-
 with open(in_file, 'r') as csvfile:
     reader = csv.DictReader(csvfile, delimiter='\t')
     n = 1
@@ -39,21 +22,21 @@ with open(in_file, 'r') as csvfile:
             print(data['result']['addressMatches'][0]['geographies']['2010 Census Blocks'][0]['TRACT'])
             print("Length: ",len(data['result']['addressMatches']))
             if len(data['result']['addressMatches']) > 0:
-                row['F5_LAT'] = data['result']['addressMatches'][0]['coordinates']['y']
-                row['F5_LNG'] = data['result']['addressMatches'][0]['coordinates']['x']
-                row['F5_TRACT'] = data['result']['addressMatches'][0]['geographies']['2010 Census Blocks'][0]['TRACT']
-                row['SOURCE'] = 'CENSUS'
+                row['lat'] = data['result']['addressMatches'][0]['coordinates']['y']
+                row['lng'] = data['result']['addressMatches'][0]['coordinates']['x']
+                row['tract'] = data['result']['addressMatches'][0]['geographies']['2010 Census Blocks'][0]['TRACT']
+                row['source'] = 'CENSUS'
                 print("Success: Census")
             else:
-                row['F5_LAT'] = '99'
-                row['F5_LNG'] = '99'
-                row['F5_TRACT'] = '99'
-                row['SOURCE'] = 'FAIL'
+                row['lat'] = '99'
+                row['lng'] = '99'
+                row['tract'] = '99'
+                row['source'] = 'FAIL'
         except:
-            row['F5_LAT'] = '99'
-            row['F5_LNG'] = '99'
-            row['F5_TRACT'] = '99'
-            row['SOURCE'] = 'FAIL'
+            row['lat'] = '99'
+            row['lng'] = '99'
+            row['tract'] = '99'
+            row['source'] = 'FAIL'
             print("fail")
         result_list.append(row)
 
